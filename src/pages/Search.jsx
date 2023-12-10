861d8a141682350e75f79a29cc0927bf43bb0827
Aung <august810.a@gmail.com>
2023-12-10 20:06:39 +0630

    refactored clickStates and clickHandler and added new state to fetch anoter array

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -149,6 +149,7 @@ const Search = () => {
   const [fourthObj, setFourthObj] = useState(null);
   const [fifthObj,setFifthObj] = useState(null);
   const [sixthObj,setSixthObj] = useState(null);
+  const [seventhObj,setSeventhObj] = useState(null);
   
   
 
@@ -358,6 +359,7 @@ const Search = () => {
               setFourthObj(filter[4]);
               setFifthObj(filter[5]);
               setSixthObj(filter[6]);
+              setSeventhObj(filter[7]);
             } 
             catch (error) {
               if (error.response) {
@@ -387,41 +389,38 @@ const Search = () => {
       setselectedTitle(e)
     }
 
-
-    const [click, setClick] = useState(Array(popularFilters?.categories?.length).fill(false));
-    const [fourthObjClick,setFourthObjClick] = useState(Array(fourthObj?.categories?.length).fill(false));
-    const [fifthObjClick,setFifthObjClick] = useState(Array(fifthObj?.categories?.length).fill(false));
-    const [sixthObjClick,setSixthObjClick] = useState(Array(sixthObj?.categories?.length).fill(false));
-
-    const clickHandler = (index,obj) => {
-      console.log(obj)
-      if( obj.id === popularFilters.id ) {
-        setClick((prevClick) => {
-          const newClick = [...prevClick];
-          newClick[index] = !newClick[index];
-          return newClick;
-          });
-      }else if(obj.id === fourthObj.id) {
-        setFourthObjClick((prevFourth)=> {
-          const newFourth = [...prevFourth];
-          newFourth[index] = !newFourth[index];
-          return newFourth
-        })
-      }else if (obj.id === fifthObj.id) {
-        setFifthObjClick((prevFifth)=>{
-          const newFifth = [...prevFifth];
-          newFifth[index] = !newFifth[index]
-          return newFifth
-        })
-      }else if (obj.id === sixthObj.id) {
-        setSixthObjClick((prevSixth)=>{
-          const newSixfth = [...prevSixth];
-          newSixfth[index] = !newSixfth[index]
-          return newSixfth
-        })
-      }
+    const [clickStates, setClickStates] = useState({
+      popular: Array(popularFilters?.categories?.length).fill(false),
+      fourthObjClick: Array(fourthObj?.categories?.length).fill(false),
+      fifthObjClick: Array(fifthObj?.categories?.length).fill(false),
+      sixthObjClick: Array(sixthObj?.categories?.length).fill(false),
+      seventhObjClick: Array(seventhObj?.categories?.length).fill(false),
+    });
+    
+    const clickHandler = (index, obj) => {
+      console.log(obj);
       
-      const category = [];
+      const { id } = obj;
+
+  setClickStates((prevStates) => {
+    const newState = { ...prevStates };
+
+    if (id === popularFilters.id) {
+      newState.popular[index] = !newState.popular[index];
+    } else if (id === fourthObj.id) {
+      newState.fourthObjClick[index] = !newState.fourthObjClick[index];
+    } else if (id === fifthObj.id) {
+      newState.fifthObjClick[index] = !newState.fifthObjClick[index];
+    } else if (id === sixthObj.id) {
+      newState.sixthObjClick[index] = !newState.sixthObjClick[index];
+    }else if (id === seventhObj.id) {
+      newState.seventhObjClick[index] = !newState.seventhObjClick[index];
+    }
+
+    return newState;
+  });
+    
+    const category = [];
       if( obj.categories[index].id !== categoriesFilter[categoriesFilter.length-1]) {
         category.push(obj.categories[index].id);
         setCategoriesFilter((prevCategoriesFilter)=> [...prevCategoriesFilter,...category])
@@ -468,13 +467,13 @@ const Search = () => {
     <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
     </svg>;
 
-    const ratings = [
-      {star:1 ,icon},
-      {star:2 ,icon},
-      {star:3 ,icon},
-      {star:4 ,icon},
-      {star:5 ,icon}
-    ];
+    // const ratings = [
+    //   {star:1 ,icon},
+    //   {star:2 ,icon},
+    //   {star:3 ,icon},
+    //   {star:4 ,icon},
+    //   {star:5 ,icon}
+    // ];
 
     
    return (
@@ -565,7 +564,7 @@ const Search = () => {
                 {!!popularFilters && popularFilters.categories && popularFilters.categories.map((filter,i)=> (
                   <div key={i} className='group flex items-center gap-2 cursor-pointer mt-3' onClick={()=> clickHandler(i,popularFilters)} >
                     <div className='border border-gray-400 group-hover:border-blue-400 bg-white rounded w-4 h-4 flex items-center justify-center'>
-                      {click[i] && svg}
+                      {clickStates.popular[i] && svg}
                     </div>
                     <span className='text-sm group-hover:text-blue-400'>{filter.name}</span>
                   </div>
@@ -582,7 +581,7 @@ const Search = () => {
                 {!!fourthObj && fourthObj.categories && fourthObj.categories.map((fourth,i)=> (
                   <div key={i} className='group flex items-center gap-2 cursor-pointer mt-3' onClick={()=> clickHandler(i,fourthObj)}>
                     <div className='border border-gray-400 group-hover:border-blue-400 bg-white rounded w-4 h-4 flex items-center justify-center'>
-                      {fourthObjClick[i] && svg}
+                      {clickStates.fourthObjClick[i] && svg}
                     </div>
                     <span className='text-sm group-hover:text-blue-400'>{fourth.name}</span>
                   </div>
@@ -599,7 +598,7 @@ const Search = () => {
                 {!!fifthObj && fifthObj.categories && fifthObj.categories.map((fifth,i)=> (
                   <div key={i} className='group flex items-center gap-2 cursor-pointer mt-3' onClick={()=> clickHandler(i,fifthObj)}>
                     <div className='border border-gray-400 group-hover:border-blue-400 bg-white rounded w-4 h-4 flex items-center justify-center'>
-                      {fifthObjClick[i] && svg}
+                      {clickStates.fifthObjClick[i] && svg}
                     </div>
                     <span className='text-sm group-hover:text-blue-400'>{fifth.name}</span>
                   </div>
@@ -616,9 +615,26 @@ const Search = () => {
                 {!!sixthObj && sixthObj.categories && sixthObj.categories.map((sixth,i)=> (
                   <div key={i} className='group flex items-center gap-2 cursor-pointer mt-3' onClick={()=> clickHandler(i,sixthObj)}>
                     <div className='border border-gray-400 group-hover:border-blue-400 bg-white rounded w-4 h-4 flex items-center justify-center'>
-                      {sixthObjClick[i] && svg}
+                      {clickStates.sixthObjClick[i] && svg}
                     </div>
                     <span className='text-sm group-hover:text-blue-400'>{sixth.name}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          }
+          {!!seventhObj && 
+            <div className='w-full'>
+              <div className='font-semibold text-sm'> 
+                {seventhObj.title}
+              </div>
+              <div className='mt-2'>
+                {!!seventhObj && seventhObj.categories && seventhObj.categories.map((seventh,i)=> (
+                  <div key={i} className='group flex items-center gap-2 cursor-pointer mt-3' onClick={()=> clickHandler(i,seventhObj)}>
+                    <div className='border border-gray-400 group-hover:border-blue-400 bg-white rounded w-4 h-4 flex items-center justify-center'>
+                      {clickStates.seventhObjClick[i] && svg}
+                    </div>
+                    <span className='text-sm group-hover:text-blue-400'>{seventh.name}</span>
                   </div>
                 ))}
               </div>
