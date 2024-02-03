3bc84b0e05e69719c08d1b1f07720fbcdf351498
Aung <august810.a@gmail.com>
2024-02-03 18:11:53 +0630

    fixed style of price and currency

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -478,7 +478,7 @@ console.log(collectedIds)
     <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
   </svg>;
 
-     
+
 
       const getData = async () => {
         setLoad_ing(true)
@@ -775,9 +775,10 @@ console.log(collectedIds)
               }
              </p>
              {data?.composite_price_breakdown?.strikethrough_amount && data?.composite_price_breakdown?.items.filter(item => item.name !== "Mobile-only price" && item.kind !== "charge").length > 0 && (
-              <div className='text-[12px] text-red-700 line-through space-x-1'>
-                <span>{data?.composite_price_breakdown?.strikethrough_amount?.currency}</span>    
-                <span>{Math.round(data?.composite_price_breakdown?.strikethrough_amount?.value)}</span>           
+              <div className='text-[12px] text-red-700 line-through'>
+                <span>
+                  {data?.composite_price_breakdown?.strikethrough_amount?.currency} {Math.round(data?.composite_price_breakdown?.strikethrough_amount?.value)}
+                </span>    
               </div>
              )}
               <div className='flex items-center justify-end gap-1'>
@@ -840,7 +841,7 @@ console.log(collectedIds)
                          <div className='flex items-center justify-between text-sm'>
                           <div>{item.name}</div>
                           <div className='space-x-1'>
-                            <span>{item.item_amount.currency}</span>
+                            <span>-  {item.item_amount.currency}</span>
                             <span>{item.item_amount.value}</span>
                           </div>
                          </div>
@@ -871,7 +872,7 @@ console.log(collectedIds)
             )}
              {data?.composite_price_breakdown.excluded_amount.value > 0 
              ? 
-             <p className='text-[12px]'>+ ${Math.round(data?.composite_price_breakdown?.excluded_amount?.value)} taxes and charges</p>
+             <p className='text-[12px]'>+{data?.composite_price_breakdown?.excluded_amount?.currency} {Math.round(data?.composite_price_breakdown?.excluded_amount?.value)} taxes and charges</p>
 
              : <p className='text-[12px]'>Includes taxes and charges</p>      
             }
